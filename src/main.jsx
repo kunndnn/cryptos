188daf1a5dfc7ec179392745e6909e72fc36bf8f
kunndnn <44980639+kunndnn@users.noreply.github.com>
2025-04-20 21:24:39 +0530

    hashrouter added

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import CoinContextProvider from "./context/CoinContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/cryptos/">
+  // <BrowserRouter>
+  <HashRouter basename="/cryptos/">
     {/* The basename prop is used to specify the base URL for all locations. */}
     <CoinContextProvider>
       <App />
     </CoinContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );

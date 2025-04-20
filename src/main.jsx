9195b35d4639af6338c65b18f23f4b9dde2931ca
kunndnn <44980639+kunndnn@users.noreply.github.com>
2025-04-20 21:16:43 +0530

    deployment code updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import CoinContextProvider from "./context/CoinContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/cryptos/">
+    {/* The basename prop is used to specify the base URL for all locations. */}
     <CoinContextProvider>
       <App />
     </CoinContextProvider>
